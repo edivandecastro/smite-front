75a3be962f4ce13c05cba0ab578faf0459b18ae7
Edivan de Castro <edivandecastro@gmail.com>
2023-01-14 00:40:54 +0044

    Alterado a store do Fornecedor para rest

--- src/App/store/Providers.js
+++ src/App/store/Providers.js
@@ -1,11 +1,14 @@
 Ext.define('Smite.store.Providers', {
   extend: 'Ext.data.Store',
 
+  xtype: 'store-provider',
+
   model: 'Smite.model.Provider',
+  pageSize: 30,
 
   proxy: {
-    type: 'ajax',
-    url: 'https://snapcode.proxy.beeceptor.com/extjs/providers',
+    type: 'rest',
+    url: 'http://localhost:3000/providers',
     reader: {
       type: 'json',
       rootProperty: 'providers',
