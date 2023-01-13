353a6809dac0d4c644be4d9f4029d59816d49518
Edivan de Castro <edivandecastro@gmail.com>
2023-01-14 00:38:26 +0044

    Implementado container dos grids da aplicação

---
+++ src/App/view/Container.js
@@ -0,0 +1,12 @@
+Ext.define('Smite.view.Container', {
+  extend: 'Ext.panel.Panel',
+
+  id: 'containerPanel',
+
+	xtype: 'smite-container',
+
+  layout: 'fit',
+  border: false,
+  height: 850,
+  margin: 0,
+});
