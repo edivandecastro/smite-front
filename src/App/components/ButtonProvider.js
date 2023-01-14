3fa1c9b6c3e367820acdff216dd1da3dbfb1acc2
Edivan de Castro <edivandecastro@gmail.com>
2023-01-14 11:44:06 +0044

    Removido requires desnecessários

--- src/App/components/ButtonProvider.js
+++ src/App/components/ButtonProvider.js
@@ -2,11 +2,6 @@ Ext.define('Smite.components.ButtonProvider', {
   extend: 'Ext.button.Split',
   xtype: 'smite-button-provider',
 
-  requires: [
-    'Smite.view.Providers',
-    'Ext.window.Window',
-  ],
-
   itemId: 'showGridProvider',
   text: 'Fornecedores',
   iconCls: 'toolbar-user-suit',
