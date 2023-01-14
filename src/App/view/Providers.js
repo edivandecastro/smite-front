af29cbe3960e40e5e91bd2aa27f7c45ed4b9e0fb
Edivan de Castro <edivandecastro@gmail.com>
2023-01-14 10:53:11 +0044

    Implementado o grid para exibir os fornecedores

---
+++ src/App/view/Providers.js
@@ -0,0 +1,25 @@
+Ext.define('Smite.view.Providers', {
+  extend: 'Ext.grid.Panel',
+  alias: 'widget.providers',
+
+  requires: ['Smite.store.Providers'],
+
+  store: 'Smite.store.Providers',
+
+  title: 'Fornecedores',
+  iconCls: 'icon-grid',
+
+  columns: [
+    { text: 'Nome', width: 100, dataIndex: 'name', sortable: true, flex: 1 },
+    { text: 'Telefone', width: 400, dataIndex: 'phone', sortable: true },
+    { text: 'E-mail', width: 400, dataIndex: 'email', sortable: true },
+    { text: 'Total Comprado', width: 400, dataIndex: 'total_purchased', sortable: true },
+  ],
+  bbar: [{
+    xtype: 'pagingtoolbar',
+    store: 'Smite.store.Providers',
+    displayInfo: true,
+    displayMsg: 'Exibindo os fornecedores {0} - {1} de {2}',
+    emptyMsg: "Sem fornecedores para exibir"
+  }],
+});
